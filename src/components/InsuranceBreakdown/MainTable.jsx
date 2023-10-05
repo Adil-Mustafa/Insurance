acefc182954f8c0f209d98bed30dd81172bc8349
Nabeel Nasir <69030093+nabeelnasir9@users.noreply.github.com>
2023-10-05 17:04:26 +0500

    tooltip updates

--- src/components/InsuranceBreakdown/MainTable.jsx
+++ src/components/InsuranceBreakdown/MainTable.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
 import leaf from "../../assets/leaf.svg";
 import "../../styles/MainTable.css";
-import Tooltips from "./Tooltips";
+import Tooltip from "@mui/material/Tooltip";
 
 const MainTable = ({ data }) => {
   return (
@@ -9,21 +10,27 @@ const MainTable = ({ data }) => {
       <h1 className="mt-coverage-heading">Coverage Details</h1>
       {data.map((item, index) => (
         <div
-          className={`mt-card-container ${index % 2 === 0 ? "mt-card-even" : "mt-card-odd"
-            }`}
+          className={`mt-card-container ${
+            index % 2 === 0 ? "mt-card-even" : "mt-card-odd"
+          }`}
           key={index}
         >
           <h2 className="mt-card-heading">
             <img src={leaf} alt="Icon" className="mt-heading-icon" />
             {item.firstHeading.includes("in network provider") ? (
-              <Tooltips
-                content="Healthcare providers with agreements 
-              with your insurance company,
-               often resulting in
-               lower out-of-pocket costs."
+              <Tooltip
+                title="Healthcare providers with agreements 
+                           with your insurance company,
+                           often resulting in
+                           lower out-of-pocket costs."
+                classes={{ tooltip: "white-tooltip" }}
+                arrow
+                interactive
+                placement="top"
+                enterTouchDelay={0}
               >
-                {item.firstHeading}
-              </Tooltips>
+                <span className="no-outline">{item.firstHeading}</span>
+              </Tooltip>
             ) : (
               item.firstHeading
             )}
@@ -32,13 +39,18 @@ const MainTable = ({ data }) => {
             {item.paragraphs.slice(0, 3).map((para) =>
               para.includes("Coinsurance") ? (
                 <p className="mt-card-paragraph">
-                  <Tooltips
-                    content="The percentage of medical costs 
-you share with your insurance 
-after meeting your deductible."
+                  <Tooltip
+                    title="The percentage of medical costs 
+                               you share with your insurance 
+                               after meeting your deductible."
+                    classes={{ tooltip: "white-tooltip" }}
+                    arrow
+                    interactive
+                    placement="top"
+                    enterTouchDelay={0}
                   >
-                    {para}
-                  </Tooltips>
+                    <span className="no-outline">{para}</span>
+                  </Tooltip>
                 </p>
               ) : (
                 <p className="mt-card-paragraph">{para}</p>
@@ -48,14 +60,19 @@ after meeting your deductible."
           <h2 className="mt-card-heading">
             <img src={leaf} alt="Icon" className="mt-heading-icon" />
             {item.secondHeading.includes("out of network provider") ? (
-              <Tooltips
-                content="Healthcare providers without agreements
-              with your insurance, potentially
-              leading to
-              higher out-of-pocket costs."
+              <Tooltip
+                title="Healthcare providers without agreements
+                           with your insurance, potentially
+                           leading to
+                           higher out-of-pocket costs."
+                classes={{ tooltip: "white-tooltip" }}
+                arrow
+                interactive
+                placement="top"
+                enterTouchDelay={0}
               >
-                {item.secondHeading}
-              </Tooltips>
+                <span className="no-outline">{item.secondHeading}</span>
+              </Tooltip>
             ) : (
               item.secondHeading
             )}
@@ -64,13 +81,18 @@ after meeting your deductible."
             {item.paragraphs.slice(3, 6).map((para) =>
               para.includes("Coinsurance") ? (
                 <p className="mt-card-paragraph">
-                  <Tooltips
-                    content="The percentage of medical costs 
-you share with your insurance 
-after meeting your deductible."
+                  <Tooltip
+                    title="The percentage of medical costs 
+                               you share with your insurance 
+                               after meeting your deductible."
+                    classes={{ tooltip: "white-tooltip" }}
+                    arrow
+                    interactive
+                    placement="top"
+                    enterTouchDelay={0}
                   >
-                    {para}
-                  </Tooltips>
+                    <span className="no-outline">{para}</span>
+                  </Tooltip>
                 </p>
               ) : (
                 <p className="mt-card-paragraph">{para}</p>
