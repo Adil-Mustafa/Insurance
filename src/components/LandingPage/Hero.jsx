d2b7fadf9d3120a22b56b115708ce84fd58c2dd7
Hafiz Muhammad Mateen <mateen.ismail17@gmail.com>
2023-10-08 19:31:40 +0500

    solve responsive issue

--- src/components/LandingPage/Hero.jsx
+++ src/components/LandingPage/Hero.jsx
@@ -10,7 +10,7 @@ function Hero() {
       <div className="w-full h-full absolute">
         <div className="w-[100%] h-full flex flex-col sm:flex-row  sm:px-14 md:px-16 lg:px-20px xl:px-24 2xl:px-28">
           {/* left side */}
-          <div className="w-full sm:w-[55%] h-full flex flex-col py-4  sm:py-10 ">
+          <div className="w-full sm:w-[60%] h-full flex flex-col py-4  sm:py-10 ">
             <div className=" hidden w-full flex justify-center sm:justify-start">
               <div className="bg-[#1363DF1A] flex gap-x-2 font-Jakarta font-semibold text-[#185A81] py-2 px-3 w-[350px] rounded-full">
                 <img src={Fire} alt="fire" />
@@ -19,10 +19,10 @@ function Hero() {
             </div>
             <div className="w-full px-3 sm:px-0 text-[#06283D] capitalize font-Jakarta text-[30px] sm:text-[30px] md:text-[30px] lg:text-[45px] xl:text-[50px] 2xl:text-[70px]">
               <h1 className=" tracking-wide font-OutFit font-bold">
-                
                 <span className="flex flex-col my-0 sm:-my-5  gap-y-0 sm:-gap-y-3">
                   <h1>
-                    your <WordStyled id="health_insurance" word="Health Insurance" />
+                    your{" "}
+                    <WordStyled id="health_insurance" word="Health Insurance" />
                   </h1>
 
                   {/* <div className='w-full flex justify-end pr-7 sm:px-44 md:-mt-2 xl:-mt-4'><img src={HeadingLine} alt="Heading line" className='w-[150px] sm:w-[400px]' /></div> */}
@@ -31,8 +31,11 @@ function Hero() {
               </h1>
             </div>
             <div className="font-Jakarta text-[#06283D99] w-full px-3 sm:pr-28 my-2">
-              <h1 className="text-[15px] sm:text-[20px]">
-              Welcome to Hauora Health, where we make Your Health Insurance simple, straightforward, and stress-free! Say goodbye to insurance jargon and confusion. Start comparing plans and take control of your coverage with confidence.
+              <h1 className="text-[15px] sm:text-[15px]">
+                Welcome to Hauora Health, where we make Your Health Insurance
+                simple, straightforward, and stress-free! Say goodbye to
+                insurance jargon and confusion. Start comparing plans and take
+                control of your coverage with confidence.
               </h1>
             </div>
             <div className=" md:my-5 2xl:my-10">
@@ -41,7 +44,7 @@ function Hero() {
               </button>
             </div>
           </div>
-          <div className="w-full sm:w-[45%] h-[200px] sm:h-full flex justify-center items-center">
+          <div className="flex items-center">
             <img
               src={HeroRightSideImage}
               alt="Right side"
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,8 +8,10 @@ import HeroPattern from "../assets/HeroPattren.png";
 import Footer from "../shared/Footer";
 import FeatureComponent from "../components/LandingPage/FeatureComponent";
 import UserReviewsComponent from "../components/LandingPage/ReviewsComponent";
+import { useMediaQuery } from "@mui/material";
 
 function Home() {
+  const isSmallScreen = useMediaQuery("(max-width: 640px)");
   return (
     <main className="w-screen h-screen relative overflow-x-hidden">
       <div className="absolute w-full h-full">
@@ -17,9 +19,14 @@ function Home() {
           <img src={HeroStar} alt="hero star" className="absolute opacity-20" />
         </div>
         <Header />
-        <section className="relative left-0 w-full h-[90%] ">
-          <Hero />
-          <div className="relative sm:absolute bottom-2 h-[62px] md:h-[140px] lg:h-[150px] xl:h-[170px] 2xl:h-[202px] sm:h-fit z-50 w-full">
+        <section className=" w-full h-[90%]">
+          <section className="h-full">
+            <Hero />
+          </section>
+          <div
+            className="z-50 w-full"
+            style={{ ...(isSmallScreen && { marginTop: 150 }) }}
+          >
             <img
               src={HeroPattern}
               alt="Hero bottom pattern"
