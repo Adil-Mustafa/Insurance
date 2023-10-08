13134c20172b4297e4fa4e764b3d062a8cc29654
Adil-Mustafa <l174471@lhr.nu.edu.pk>
2023-10-08 12:25:01 +0500

    minor heading fixes

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -35,19 +35,19 @@ function Header() {
           <div className="flex items-center gap-x-10">
             <div className="flex items-center cursor-pointer">
               <img src={AppLogo} alt="Logo" />
-              <h1 className="text-[16px] sm:text-[22px] text-[#06283D] font-bold font-Jakarta tracking-wide">
+              <h1 className="xl:text-[23px] sm:text-[16px] text-[#06283D] font-bold font-OutFit tracking-wide">
                 HAUOURA HEALTH
               </h1>
             </div>
             <div className="hidden sm:block">
               <ul className="flex gap-x-10 text-[#06283D] text-[18px] font-Jakarta">
-                <li className="cursor-pointer hover:text-[#47A5DC]">
-                  Our Plans
+                <li className="cursor-pointer font-bold hover:text-[#47A5DC]">
+                  All Plans
                 </li>
-                <li className="cursor-pointer hover:text-[#47A5DC]">
+                <li className="cursor-pointer font-bold hover:text-[#47A5DC]">
                   About us
                 </li>
-                <li className="cursor-pointer hover:text-[#47A5DC]">
+                <li className="cursor-pointer font-bold hover:text-[#47A5DC]">
                   Contact us
                 </li>
               </ul>
@@ -55,7 +55,7 @@ function Header() {
           </div>
           <div className="">
             <button className="hidden sm:block rounded-[8px] bg-[#47A5DC] py-4 px-7 font-Jakarta text-white hover:bg-[#6BB7E2] duration-150 drop-shadow-2xl shadow-black/20 shadow-2xl">
-              Compare Plan
+              Compare Plans
             </button>
             <img
               src={MenuIcon}
